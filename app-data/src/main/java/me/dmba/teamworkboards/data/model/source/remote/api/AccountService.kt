289f9ebe8f578670ca653298f317714ca863b1e1
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-07-16 18:50:36 +0200

    Add basic remote data source

---
+++ app-data/src/main/java/me/dmba/teamworkboards/data/model/source/remote/api/AccountService.kt
@@ -0,0 +1,7 @@
+package me.dmba.teamworkboards.data.model.source.remote.api
+
+/**
+ * Created by dmba on 7/16/18.
+ */
+internal interface AccountService {
+}
---
+++ app-data/src/main/java/me/dmba/teamworkboards/data/model/source/remote/datasource/RemoteTaskDataSource.kt
@@ -0,0 +1,10 @@
+package me.dmba.teamworkboards.data.model.source.remote.datasource
+
+import me.dmba.teamworkboards.data.model.access.TaskRepo
+import javax.inject.Inject
+
+/**
+ * Created by dmba on 7/16/18.
+ */
+internal class RemoteTaskDataSource @Inject constructor() : TaskRepo {
+}
