94c36133506e54120e37011e7dddaee771a2a2c8
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-07-17 00:15:30 +0200

    Add fetch account info to account repo

--- app-data/src/main/java/me/dmba/teamworkboards/data/model/access/AccountRepo.kt
+++ app-data/src/main/java/me/dmba/teamworkboards/data/model/access/AccountRepo.kt
@@ -1,7 +1,13 @@
 package me.dmba.teamworkboards.data.model.access
 
+import io.reactivex.Single
+import me.dmba.teamworkboards.data.model.entity.Account
+
 /**
  * Created by dmba on 7/16/18.
  */
 interface AccountRepo {
+
+    fun getAccountDetails(): Single<Account>
+
 }
