c16e99c2b4ef5d00efbdf803d1130e0056eee289
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-07-16 22:23:03 +0200

    Add login contract and presenter

---
+++ app-domain/src/main/java/me/dmba/teamworkboards/domain/impl/LoginPresenter.kt
@@ -0,0 +1,48 @@
+package me.dmba.teamworkboards.domain.impl
+
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import me.dmba.teamworkboards.common.utils.addTo
+import me.dmba.teamworkboards.data.model.access.AuthRepo
+import me.dmba.teamworkboards.domain.base.DisposablePresenter
+import me.dmba.teamworkboards.domain.contract.LoginContract
+import javax.inject.Inject
+
+/**
+ * Created by dmba on 7/16/18.
+ */
+internal class LoginPresenter @Inject constructor(
+
+    private val view: LoginContract.View,
+
+    private val navigator: LoginContract.Navigator,
+
+    private val authRepo: AuthRepo
+
+) : DisposablePresenter(), LoginContract.Presenter {
+
+    override fun onViewCreated() {
+        if (authRepo.hasApiToken) {
+            navigator.goToBoards()
+        } else {
+            view.loadLoginPage(authRepo.loginUrl)
+        }
+    }
+
+    override fun onReceiveLoginCode(code: String) {
+        authRepo.authenticate(code)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::onSuccess, this::onError)
+            .addTo(disposables)
+    }
+
+    private fun onSuccess() {
+        navigator.goToBoards()
+    }
+
+    private fun onError(throwable: Throwable) {
+        view.showLoginError()
+    }
+
+}
