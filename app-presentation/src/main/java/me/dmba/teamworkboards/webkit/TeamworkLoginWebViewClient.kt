de7799055ab942b7ec80d425e0acd44bff3ef69e
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-07-17 08:24:30 +0200

    Add teamwork login webview client

---
+++ app-presentation/src/main/java/me/dmba/teamworkboards/webkit/TeamworkLoginWebViewClient.kt
@@ -0,0 +1,32 @@
+package me.dmba.teamworkboards.webkit
+
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+import me.dmba.teamworkboards.data.model.access.AuthRepo
+import me.dmba.teamworkboards.utils.KEY_CODE
+import javax.inject.Inject
+
+/**
+ * Created by dmba on 7/17/18.
+ */
+class TeamworkLoginWebViewClient @Inject constructor(
+
+    private val authRepo: AuthRepo
+
+) : WebViewClient() {
+
+    val onReceiveLoginCode: Subject<String> = BehaviorSubject.create()
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        return if (request.url.authority == authRepo.loginRedirectUrl) {
+            onReceiveLoginCode.onNext(request.url.getQueryParameter(KEY_CODE))
+            true
+        } else {
+            false
+        }
+    }
+
+}
